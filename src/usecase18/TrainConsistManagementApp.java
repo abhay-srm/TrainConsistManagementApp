be2c2b44a81f7a1ba6127f262e5450340e0fb376
abhay-srm <aa1716@srmist.edu.in>
2026-04-17 23:16:51 +0530

    Added Use Case 18

---
+++ src/usecase18/TrainConsistManagementApp.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class TrainConsistManagementApp{
+    public static void main(String[] args){
+        System.out.println("=====================================");
+        System.out.println("UC18 - Linear Search for Bogie ID");
+        System.out.println("=====================================");
+
+        String[] bogieIds = {"BG101","BG205","BG309","BG412","BG550"};
+        String searchId = "BG309";
+
+        System.out.println("\nAvailable Bogie IDs: ");
+        for(String id : bogieIds){
+            System.out.println(id);
+        }
+        boolean found = false;
+        for(String id : bogieIds){
+            if(id.equals(searchId)){
+                found = true;
+                break;
+            }
+        }
+        if(found){
+            System.out.println("\nBogie " +searchId+ " found in train consist.");
+        }
+        else{
+            System.out.println("\nBogie" +searchId+ " not found in train consist.");
+        }
+        System.out.println("\nUC18 search completed...");
+    }
+}
